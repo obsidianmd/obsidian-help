49faed37ab3e7322bb1e40cce2cfb6a0deded5ac
Steph Ango <10565871+kepano@users.noreply.github.com>
2024-03-02 12:42:18 -0800

    Use registerMarkdownPostProcessor

--- en/publish.js
+++ en/publish.js
@@ -1,6 +1,11 @@
-document.querySelector('.select_location').addEventListener('change', function() {
-    var url = this.value;
-    if (url) {
+publish.registerMarkdownPostProcessor(async (el, ctx) => {
+  const selectElement = el.querySelector('.select_location');
+  if (selectElement) { // Check if the element exists
+    selectElement.addEventListener('change', function() {
+      var url = this.value;
+      if (url) {
         window.location = url;
-    }
+      }
+    });
+  }
 });
